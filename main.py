ca90ceafae07f157817117a7eaadd50717c1602f
RoketFlame <danil.slinchuk@gmail.com>
2021-04-28 18:26:30 +0300

    try again...

--- main.py
+++ main.py
@@ -507,4 +507,5 @@ if __name__ == '__main__':
     api.add_resource(news_resources.NewsListResource, '/api/news')
     api.add_resource(news_resources.NewsResource, '/api/news/<int:news_id>')
     app.register_blueprint(api_news.blueprint)
-    waitress.serve(app, host='127.0.0.1', port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
