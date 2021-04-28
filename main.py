c2269ac00da95537775811cd6729ca7fbf4f1481
RoketFlame <danil.slinchuk@gmail.com>
2021-04-28 18:57:31 +0300

    again...

--- main.py
+++ main.py
@@ -508,4 +508,4 @@ if __name__ == '__main__':
     api.add_resource(news_resources.NewsResource, '/api/news/<int:news_id>')
     app.register_blueprint(api_news.blueprint)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    waitress.serve(app, host='0.0.0.0', port=port)
